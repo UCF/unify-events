4f68403608e839244bd2a6325887eb9b5dde996b
Jared Lang <kaptainlange@gmail.com>
2010-09-16 19:46:32 -0400

    Added handler overrides for 500, 404

--- src/urls.py
+++ src/urls.py
@@ -5,6 +5,10 @@ urlpatterns = patterns('',
 	url(r'^(?P<calendar>[\w]+)/$', include('events.urls')),
 )
 
+from django.views.generic.simple import direct_to_template
+handler500 = lambda r: direct_to_template(r, template='500.html')
+handler404 = lambda r: direct_to_template(r, template='404.html')
+
 if settings.DEBUG:
 	urlpatterns += patterns('',
 		(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
