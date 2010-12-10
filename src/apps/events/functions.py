e5f16dc67ed621161d0e88f60c8e6dd4d8006724
Jared Lang <kaptainlange@gmail.com>
2010-12-09 22:13:23 -0500

    Fixed sluggify function.

--- src/apps/events/functions.py
+++ src/apps/events/functions.py
@@ -1,6 +1,7 @@
 def sluggify(original):
 	"""docstring for sluggify"""
 	import re
-	slug  = original.lower().replace(' ', '-')
-	slug  = re.sub("[^A-Za-z1-9\s\-]", '', slug)
+	slug  = original.lower().strip()
+	slug  = re.sub("[\s]+", '-', slug)
+	slug  = re.sub("[^a-z1-9\s\-]", '', slug)
 	return slug
