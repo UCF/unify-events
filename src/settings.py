eed5f433ee4eaedb9055057a9039232aceddd58c
Jared Lang <jslang@mail.ucf.edu>
2011-10-05 16:01:20 -0400

    Updated template loader to fix DeprecatedWarnings.

--- src/settings.py
+++ src/settings.py
@@ -41,7 +41,7 @@ MEDIA_URL = STATIC_URL
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
 	'django.template.loaders.filesystem.Loader',
-	'django.template.loaders.app_directories.load_template_source',
+	'django.template.loaders.app_directories.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
