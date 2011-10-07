3d494976a5067ec735ef43ad4074b589f98590d6
Christopher Conover <clcono@gmail.com>
2011-10-07 11:07:20 -0400

    Continued to develop manage pages design

--- src/apps/events/views/manager/__init__.py
+++ src/apps/events/views/manager/__init__.py
@@ -1,19 +1,48 @@
 from django.contrib.auth.decorators  import login_required
 from django.views.generic.simple     import direct_to_template
-from datetime                        import datetime
+from datetime                        import datetime, timedelta, date
 from ..events.models                 import Event, Calendar
 from django.contrib                  import messages
+from django.http                     import HttpResponseRedirect
+from django.core.urlresolvers        import reverse
+
+MDAYS = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 @login_required
-def manage(request, calendar_id = None):
-	ctx  = {'events':None,'calendar':None}
+def manage(request, _date=None, calendar_id = None):
+	ctx  = {
+		'events'  :None,
+		'calendar':None,
+		'dates':{
+			'prev_day'  : None,
+			'prev_month': None,
+			'today'     : None,
+			'next_day'  : None,
+			'next_month': None,
+			'realative' : None,
+		}
+	}
 	tmpl = 'events/manager/manage.html'
 
+	# Make sure check their profile when they
+	# log in for the first time
 	if request.user.first_login:
 		return HttpResponseRedirect(reverse('accounts-profile'))
+	
+	# Date navigation
+	ctx['dates']['today'] = date.today()
+	if _date is not None:
+		ctx['dates']['relative'] = datetime(*[int(i) for i in _date.split('-')])
+	else:
+		ctx['dates']['relative'] = ctx['dates']['today']
+
+	ctx['dates']['prev_day']   = str(ctx['dates']['relative'] - timedelta(days=1))
+	ctx['dates']['prev_month'] = str(ctx['dates']['relative'] - timedelta(days=MDAYS[ctx['dates']['today'].month]))
+	ctx['dates']['next_day']   = str(ctx['dates']['relative'] + timedelta(days=1))
+	ctx['dates']['next_month'] = str(ctx['dates']['relative'] + timedelta(days=MDAYS[ctx['dates']['today'].month]))
 
 	if calendar_id is None: # Upcoming events
-		ctx['events'] = Event.objects.filter(instances__start__gte = datetime.now())
+		ctx['events'] = request.user.owned_events.filter(instances__start__gte = ctx['dates']['today'])
 	else:
 		try:
 			ctx['calendar'] = Calendar.objects.get(pk = calendar_id)
@@ -21,5 +50,5 @@ def manage(request, calendar_id = None):
 			messages.error('Calendar does not exist')
 		else:
 			ctx['events'] = ctx['calendar'].events.all()
-			
+
 	return direct_to_template(request,tmpl,ctx)
