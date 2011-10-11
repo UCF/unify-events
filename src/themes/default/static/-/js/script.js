621c4004d754c2f7b22beef90fecb8dfcef216a2
Christopher Conover <clcono@gmail.com>
2011-10-11 15:25:22 -0400

    Added JS to power calendar selector

--- src/themes/default/static/-/js/script.js
+++ src/themes/default/static/-/js/script.js
@@ -6,7 +6,6 @@ $().ready(function() {
 	// Other Actions Nav Dropdown
 	$('#other_actions li:gt(0)')
 		.css('visibility','hidden')
-
 	$('#other_actions li')
 		.hover(
 			function() {
@@ -16,4 +15,13 @@ $().ready(function() {
 				$('#other_actions li:gt(0)').css('visibility', 'hidden')
 			}
 		);
+	
+	// Calendar Selector
+	$('#calendar_selector')	
+		.submit(function() {
+			var form     = $(this);
+			var selector = $(this).find('select');
+			window.location.href = selector.val();
+			return false;
+		})
 });
