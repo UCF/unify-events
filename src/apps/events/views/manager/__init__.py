f7b35fbf06961c9226157e73b724a3ee3d74bfd1
Christopher Conover <clcono@gmail.com>
2011-10-11 11:41:46 -0400

    Added LDAP search view

--- src/apps/events/views/manager/__init__.py
+++ src/apps/events/views/manager/__init__.py
@@ -3,11 +3,14 @@ from django.views.generic.simple     import direct_to_template
 from datetime                        import datetime, timedelta, date
 from events.models                   import Event, Calendar
 from django.contrib                  import messages
-from django.http                     import HttpResponseRedirect
+from django.http                     import HttpResponseRedirect,HttpResponse
 from django.core.urlresolvers        import reverse
 from events.forms.manager            import EventForm,EventInstanceForm
 from django.forms.models             import modelformset_factory
 from django.db.models                import Q
+from util                            import LDAPHelper
+from django.conf                     import settings
+from django.utils                    import simplejson
 
 MDAYS = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
@@ -57,3 +60,36 @@ def manage(request, _date=None, calendar_id = None):
 			ctx['events'] = ctx['calendar'].events.all()
 
 	return direct_to_template(request,tmpl,ctx)
+
+@login_required
+def search_user(request,lastname,firstname=None):
+	LDAP_RESULT_LIMIT = 10
+	results           = []
+	filter_param      = lastname
+	filter_string     = '(sn=%s*)'
+	
+	if firstname is not None:
+		filter_param  = (lastname,firstname)
+		filter_string = '(&(sn=%s*)(givenName=%s*))'
+
+	try:
+		ldap_helper = LDAPHelper()
+		LDAPHelper.bind(ldap_helper.connection,settings.LDAP_NET_SEARCH_USER,settings.LDAP_NET_SEARCH_PASS)
+		ldap_results = LDAPHelper.search(ldap_helper.connection,filter_param,filter_string)
+		
+		for ldap_result in ldap_results:
+			try:
+				results.append({
+					'lastname' :LDAPHelper.extract_lastname(ldap_result),
+					'firstname':LDAPHelper.extract_firstname(ldap_result),
+					'username' :LDAPHelper.extract_username(ldap_result),
+					})
+			except LDAPHelper.MissingAttribute:
+				pass
+			if len(results) == LDAP_RESULT_LIMIT: break
+
+	except Exception, e:
+		# Whatever happens, always return a JSON result
+		print str(e)
+		pass
+	return HttpResponse(simplejson.dumps(results),mimetype='application/json')
