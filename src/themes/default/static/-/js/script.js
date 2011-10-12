4a3a04dbefe132114ffbcae58db8fc3a8cb55281
Christopher Conover <clcono@gmail.com>
2011-10-12 16:54:18 -0400

    Added extend_formset and retract_formset functions. Added buttons and JS to extend and retract event instance formset

--- src/themes/default/static/-/js/script.js
+++ src/themes/default/static/-/js/script.js
@@ -1,5 +1,32 @@
 var sluggify = function(s) {return s.toLowerCase().replace(/\s/g, '-').replace(/[^A-Za-z-]/, '');}
 
+/*
+ * Dynamically add items to formsets.
+ * From: http://stackoverflow.com/questions/501719/dynamically-adding-a-form-to-a-django-formset-with-ajax
+ *
+ */
+function extend_formset(selector, type) {
+    var newElement = $(selector).clone(true);
+    var total = $('#id_' + type + '-TOTAL_FORMS').val();
+    newElement.find(':input').each(function() {
+        var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
+        var id = 'id_' + name;
+        $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
+    });
+    newElement.find('label').each(function() {
+        var newFor = $(this).attr('for').replace('-' + (total-1) + '-','-' + total + '-');
+        $(this).attr('for', newFor);
+    });
+    total++;
+    $('#id_' + type + '-TOTAL_FORMS').val(total);
+    $(selector).after(newElement);
+}
+function retract_formset(selector, type) {
+    var total = $('#id_' + type + '-TOTAL_FORMS').val();
+    $(selector).remove();
+    $('#id_' + type + '-TOTAL_FORMS').val(total - 1);
+}
+
 $().ready(function() {
 	
 	// EventInstance DatePickers
