34a1e52ed8347e4de552da92d0f112c58d64dd09
Jared Lang <jslang@mail.ucf.edu>
2011-10-14 13:44:44 -0400

    Documentation updates, and code rearranging.

--- src/apps/events/views/calendar.py
+++ src/apps/events/views/calendar.py
@@ -5,25 +5,41 @@ from django.template             import TemplateDoesNotExist
 from datetime                    import datetime, timedelta
 from django.shortcuts            import get_object_or_404
 from django.views.generic.simple import direct_to_template
+from time                        import gmtime, time
 from events.models               import *
 from events.functions            import format_to_mimetype
 
-# http://events.ucf.edu/athletics/today
-# http://events.ucf.edu/athletics/this-month
-# http://events.ucf.edu/athletics/this-year
-# http://events.ucf.edu/athletics/2010/
-# http://events.ucf.edu/athletics/2010/01/
-# http://events.ucf.edu/athletics/2010/01/10.(rss|json|html|xml|etc)
 
-# Create your views here.
+def calendar_widget(request, calendar, year, month):
+	"""Outputs calendar widget html via http response"""
+	from events.templatetags import widgets
+	calendar = get_object_or_404(Calendar, slug=calendar)
+	
+	try: # Convert applicable arguments to integer
+		year  = int(year) if year is not None else year
+		month = int(month) if month is not None else month
+	except ValueError:
+		raise Http404
+	
+	html = widgets.calendar_widget(calendar, year, month)
+	return HttpResponse(html)
+
+
 def calendar(request, calendar, format=None):
+	"""Main calendar page, displaying an aggregation of events such as upcoming
+	events, featured events, etc.
+	"""
 	calendar = get_object_or_404(Calendar, slug=calendar)
 	now      = gmtime()
 	start    = datetime(now.tm_year, now.tm_mon, now.tm_mday)
 	end      = start + timedelta(days=5)
 	
 	todays_events   = calendar.find_event_instances(start, end)
-	featured_events = calendar.find_event_instances(start, start + timedelta(weeks=4), calendar.featured_instances)
+	featured_events = calendar.find_event_instances(
+		start,
+		start + timedelta(weeks=4),
+		calendar.featured_instances
+	)
 	
 	events          = todays_events.order_by('start', 'event__title')
 	featured_events = featured_events.order_by('start', 'event__title')[:5]
@@ -42,6 +58,7 @@ def calendar(request, calendar, format=None):
 
 
 def event(request, calendar, instance_id, format=None):
+	"""Event instance page that serves as the public interface for events."""
 	calendar = get_object_or_404(Calendar, slug=calendar)
 	try:
 		event = calendar.events_and_subs.get(pk=instance_id)
@@ -117,9 +134,6 @@ def auto_listing(request, calendar, year=None, month=None, day=None, format=None
 	return listing(request, calendar, start, end, format)
 
 
-from time import gmtime, time
-ONE_DAY = 86400
-
 def named_listing(request, calendar, type, format=None):
 	"""Handles named event listings, such as today, this-month, or this-year."""
 	f = {
@@ -135,6 +149,9 @@ def named_listing(request, calendar, type, format=None):
 
 
 def range_listing(request, calendar, start, end, format=None):
+	"""Generates an event listing for the date ranges provided through start
+	and end.
+	"""
 	from datetime import datetime
 	date  = lambda d: datetime(*[int(i) for i in d.split('-')])
 	start = date(start)
@@ -150,14 +167,14 @@ def todays_listing(request, calendar, format=None):
 
 
 def tomorrows_listing(request, calendar, format=None):
-	"""Generates event listing for the current day"""
-	now = gmtime(time() + ONE_DAY)
+	"""Generates event listing for tomorrows events"""
+	now = gmtime(time() + 86400)
 	year, month, day = now.tm_year, now.tm_mon, now.tm_mday
 	return auto_listing(request, calendar, year, month, day, format)
 
 
 def weeks_listing(request, calendar, format=None):
-	"""Generates event listing for the current week"""
+	"""Generates an event list for this weeks events"""
 	today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 	start = today - timedelta(days=today.weekday())
 	end   = start + timedelta(weeks=1)
@@ -165,29 +182,15 @@ def weeks_listing(request, calendar, format=None):
 
 
 def months_listing(request, calendar, format=None):
-	"""Generates event listing for the current month"""
+	"""Generate event list for the current month"""
 	now = gmtime()
 	year, month, day = now.tm_year, now.tm_mon, None
 	return auto_listing(request, calendar, year, month, day, format)
 
 
 def years_listing(request, calendar, format=None):
-	"""Generates event listing for the current year"""
+	"""Generate event listing for this year"""
 	now = gmtime()
 	year, month, day = now.tm_year, None, None
 	return auto_listing(request, calendar, year, month, day, format)
-
-
-def calendar_widget(request, calendar, year, month):
-	"""Outputs calendar widget html via http response"""
-	from events.templatetags import widgets
-	calendar = get_object_or_404(Calendar, slug=calendar)
 	
-	try: # Convert applicable arguments to integer
-		year  = int(year) if year is not None else year
-		month = int(month) if month is not None else month
-	except ValueError:
-		raise Http404
-	
-	html     = widgets.calendar_widget(calendar, year, month)
-	return HttpResponse(html)
