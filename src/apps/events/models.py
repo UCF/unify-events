08519bf3da66b0e8fc42c8c3bdd1baeec698d129
Christopher Conover <clcono@gmail.com>
2011-10-17 16:17:28 -0400

    Distinct calendars_include_submitted

--- src/apps/events/models.py
+++ src/apps/events/models.py
@@ -33,7 +33,7 @@ def calendars_include_submitted(self):
 	return Calendar.objects.filter(
 		models.Q(creator=self)|
 		models.Q(editors=self)|
-		models.Q(events__creator=self)).order_by('name')
+		models.Q(events__creator=self)).order_by('name').distinct()
 setattr(User,'calendars_include_submitted', property(calendars_include_submitted))
 
 
@@ -72,6 +72,8 @@ class Event(Base):
 	settings     = SettingsField(default=Settings.default, null=True, blank=True)
 	creator      = models.ForeignKey(User, related_name='owned_events', null=True)
 	image        = models.FileField(upload_to=_settings.FILE_UPLOAD_PATH,null=True)
+	tags         = models.ManyToManyField('Tag', related_name='events')
+	categories   = models.ManyToManyField('Category', related_name='events')
 
 	def pull_updates(self):
 		"""Updates this Event with information from the event it was created 
@@ -134,6 +136,12 @@ class Event(Base):
 	class Meta:
 		ordering = ['instances__start']
 
+class Tag(Base):
+	name = models.CharField(max_length = 100)
+
+class Category(Base):
+	name   = models.CharField(max_length = 100)
+	parent = models.ForeignKey('EventInstance', related_name='subcategories', null=True, blank=True)
 
 class EventInstance(Base):
 	"""Object which describes the time and place that an event is occurring"""
