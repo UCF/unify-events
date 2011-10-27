f513b8219fe6e570657ae657ab110029a74e2d54
Jared Lang <jslang@mail.ucf.edu>
2011-10-27 11:01:19 -0400

    Modified minify assets command and middleware to play nice with apache and development.

--- src/apps/events/middleware.py
+++ src/apps/events/middleware.py
@@ -4,9 +4,13 @@ from django.conf            import settings
 import re
 
 class Minifier:
+	active = False
+	
 	def __init__(self):
-		settings.ORIGINAL_MEDIA_ROOT = settings.MEDIA_ROOT
-		if settings.USE_MINIFY:
-			settings.MEDIA_ROOT = settings.MEDIA_ROOT + '-min'
+		if settings.MINIFY and not Minifier.active:
 			call_command('generate-minified-assets')
+			Minifier.active     = True
+			settings.MEDIA_URL  = settings.MEDIA_URL + 'min/'
+			settings.MEDIA_ROOT = settings.MEDIA_ROOT + '/min'
+			
 		raise MiddlewareNotUsed
