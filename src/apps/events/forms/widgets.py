e13fa49526fed6dd92048f5dafc915540daf96a8
Chris Conover <clcono@gmail.com>
2011-11-01 15:02:37 -0400

    Fixed template error when editors is None

--- src/apps/events/forms/widgets.py
+++ src/apps/events/forms/widgets.py
@@ -14,9 +14,10 @@ class InlineLDAPSearch(forms.Widget):
 		html.append('<ul class="actions"><li><a class="add">Add</a></li>')
 		html.append('<li><a class="remove">Remove</a></li></ul>')
 		html.append('<select multiple="multiple" class="selections" name="%s">' % name)
-		for user_id in value:
-			user    = User.objects.get(pk=user_id)
-			display = user.first_name + ' ' + user.last_name + ' (' + user.username + ')'
-			html.append('<option value="%d">%s</option>' % (user_id, display))
+		if value is not None:
+			for user_id in value:
+				user    = User.objects.get(pk=user_id)
+				display = user.first_name + ' ' + user.last_name + ' (' + user.username + ')'
+				html.append('<option value="%d">%s</option>' % (user_id, display))
 		html.append('</select></div>')
 		return mark_safe(u'\n'.join(html))
