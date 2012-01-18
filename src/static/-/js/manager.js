60ef9bd0c7fd776de50a8297134a0e56b69d030c
Douglas Beck <dbeck@douglasbeck.com>
2012-01-18 10:40:57 -0500

    more stuff for the recurring dialog, there's so much.  getting close

--- src/static/-/js/manager.js
+++ src/static/-/js/manager.js
@@ -21,13 +21,99 @@ $('#title input')
 	});
 
 
-//$('#repeat-dialog').dialog({ modal  : true });
 $('.buttons').buttonset();
+$('#dialog-date').datepicker({ dateFormat: 'mm/d/y'});
+
+/******************************************************************************\
+  Prompt user to define / edit an event's when-where recurrence
+\******************************************************************************/
+Unify.recurrence = function(ww){
+	
+	// parse the event's recurrence data
+	var data = JSON.parse($('#recurrence').val());
+	if(data){
+		$(ww).data(data);
+	} else {
+		// parse data from event form and fill data with defaults
+		var txt = $(ww).find('.date').val();
+		var date = $.datepicker.parseDate('mm/dd/y', txt);
+		var dayNames = ['Sun', 'Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat'];
+		var day = dayNames[date.getDay()];
+		var until = new Date();
+		var six_mo = 1000*60*60*24*183;
+		until.setTime(date.getTime() + six_mo);
+		var summary = "Repeats every " + day + " until " + $.datepicker.formatDate('M d, yy', until);
+		$(ww).data({
+			'date'     : date,
+			'interval' : {
+				'repeats' : 'week',
+				'every'   : { 'week' : [day], 'month': 'dayofweek' },
+				'until'   : $.datepicker.formatDate('mm/dd/y', until)
+			},
+			'summary'  : summary
+		});
+	}
+
+	// init recur dialog with data (repeats, repeats-on, ends, summary)
+	$('#repeats-week, #repeats-month').hide();
+	$('#repeats-' + $(ww).data('interval').repeats).show();
+	$('#repeat-dialog input').removeAttr('checked').removeAttr('selected');
+	$('#repeat-dialog label').removeClass('ui-state-active');
+	$('#radio' + $(ww).data().interval.repeats).attr('checked', 'checked');
+	$('label[for=radio' + $(ww).data().interval.repeats + ']').addClass('ui-state-active');
+	var days = $(ww).data('interval')['every']['week'];
+	for (var i = 0; i < days.length; i++){
+		$('#repeat-dialog label.' + days[i]).addClass('ui-state-active');
+		$('#repeat-dialog input.' + days[i]).attr('checked', 'checked');
+	}
+	$('#dialog-date').val($(ww).data().interval.until);
+	$('#repeat-summary').html($(ww).data('summary'));
+	
+
+	$('#repeat-dialog').click(function(){
+		// parse the repeat form and update the summary
+		$('#week-interval-days input:checked').each(function(){
+				days.push($(this).val());
+			});
+		
+		$('#repeat-summary').html($(ww).data('summary'));
+		return;
+	});
+
+	// validate, strinify data into recurrence input
+	var ok = {
+		text : 'Ok',
+		click : function(){
+			console.log('validation!');
+			var data = JSON.stringify($(ww).data());
+			$('#recurrence').val(data);	
+			$('#repeat-dialog').dialog("close");
+		}
+	};
+
+	var cancel = {
+		text : 'Cancel',
+		click : function(){
+			$(ww).find('.rec input').removeAttr('checked');
+			$('#repeat-dialog').dialog("close");
+		}
+	};
+
+	$('#repeat-dialog').dialog({
+		modal     : true,
+		minWidth  : 475,
+		buttons   : [ok, cancel],
+		resizable : false
+	});
+}
 
 
 $('.when-where').each(function(){
+	
 	//date
-	$(this).find('.date').datepicker();
+	var date = $(this).find('.date');
+	date.datepicker({ dateFormat: 'mm/dd/y' });
+	$(this).data('date', date.val());
 
 	//time
 	var end = $(this).find('.end');
@@ -52,8 +138,9 @@ $('.when-where').each(function(){
 	});
 
 	//recurrence
+	var when_where_instance = this;
 	$(this).find('.rec').click(function(){
-		return;
+		Unify.recurrence(when_where_instance);
 	});
 });
 
