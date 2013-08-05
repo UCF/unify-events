a969fda50e3eba330a6be79b94f03576d18e29f7
Jo Greybill <jogreybill@gmail.com>
2013-08-05 16:48:53 -0400

    Bug fixes for event when/where cloning

--- static/js/script.js
+++ static/js/script.js
@@ -45,64 +45,75 @@ $('document').ready(function() {
      * Calendar grid carousels
      **/
     $('.calendar-slider').on('slid', function() {
-    	var firstItem = $('.calendar-slider .carousel-inner .item:first-child'),
-    		lastItem  = $('.calendar-slider .carousel-inner .item:last-child'),
-    		controlNext = $('.calendar-slider .pager .next'),
-    		controlPrev = $('.calendar-slider .pager .previous'),
+        var firstItem = $('.calendar-slider .carousel-inner .item:first-child'),
+            lastItem  = $('.calendar-slider .carousel-inner .item:last-child'),
+            controlNext = $('.calendar-slider .pager .next'),
+            controlPrev = $('.calendar-slider .pager .previous'),
             sliderID = $(this).attr('id');
     
-    	if (firstItem.hasClass('active')) {
-    		controlPrev
-    			.addClass('disabled')
-    			.find('a')
-    				.attr('href', '#');
-    	}
-    	else {
-    		controlPrev
-    			.removeClass('disabled')
-    			.find('a')
-    				.attr('href', '#' + sliderID);
-    	}
-    	if (lastItem.hasClass('active')) {
-    		controlNext
-    			.addClass('disabled')
-    			.find('.carousel-control.right')
-    				.attr('href', '#');
-    	}
-    	else {
-    		controlNext
-    			.removeClass('disabled')
-    			.find('.carousel-control.right')
-    				.attr('href', '#' + sliderID);
-    	}
+        if (firstItem.hasClass('active')) {
+            controlPrev
+            .addClass('disabled')
+                .find('a')
+                    .attr('href', '#');
+        }
+        else {
+            controlPrev
+                .removeClass('disabled')
+                .find('a')
+                    .attr('href', '#' + sliderID);
+        }
+        if (lastItem.hasClass('active')) {
+            controlNext
+                .addClass('disabled')
+                .find('.carousel-control.right')
+                    .attr('href', '#');
+        }
+        else {
+            controlNext
+                .removeClass('disabled')
+                .find('.carousel-control.right')
+                    .attr('href', '#' + sliderID);
+        }
     });
     
     $('.disabled').click(function(e) {
-    	e.preventDefault();
+        e.preventDefault();
     });
     
     
     /**
      * Date/Timepicker Init
      **/
-    $('.field-date').datepicker({
-        format:'yyyy-mm-dd'
-    });
-    var date = new Date();
-    var time = date.getHours() + ':' + date.getMinutes();
-    $('.field-time')
-        .each(function(){
-            $(this)
-                .attr('placeholder', time)
-                .next('span.add-on')
-                .andSelf()
-                .wrapAll('<div class="input-append bootstrap-timepicker" />');
-        }).timepicker({
-            showMeridian: false,
-            defaultTime: false
+    var initiateDatePickers = function(field) {
+        field.datepicker({
+            format:'yyyy-mm-dd'
         });
+    };
+    var initiateTimePickers = function(field) {
+        var date = new Date();
+        var time = date.getHours() + ':' + date.getMinutes();
+        field
+            .each(function(){
+                // Wrap each timepicker input if this field isn't a clone
+                if ($(this).parent().hasClass('bootstrap-timepicker') === false) {
+                    $(this)
+                        .attr('placeholder', time)
+                        .next('span.add-on')
+                        .andSelf()
+                        .wrapAll('<div class="input-append bootstrap-timepicker" />');
+                }
+            }).timepicker({
+                showMeridian: false,
+                defaultTime: false
+            });
+    };
+    $(document).ready(function() {
+        initiateDatePickers($('.field-date'));
+        initiateTimePickers($('.field-time'));
+    });
         
-        
+
     /**
      * WYSIWIG Textarea Init
      **/
@@ -165,29 +176,36 @@ $('document').ready(function() {
             else {
                 cloneableWrap.find('.remove-instance').removeClass('hidden');
             }
-        }
+        };
 
         // Delete a cloneable element
         var deleteForm = function(btn, prefix) {
             var formCount = parseInt($('#id_' + prefix + '-TOTAL_FORMS').val(), 10);
             if (formCount > 1) {
-                // Delete the item/form
-                $(btn).parents('.cloneable').remove();
-                var forms = $('.cloneable'); // Get all the cloneable items
-                // Update the total number of cloneables (1 less than before)
-                $('#id_' + prefix + '-TOTAL_FORMS').val(forms.length);
-                var i = 0;
-                // Go through the cloneables and set their indeces, names and IDs
-                for (formCount=forms.length; i<formCount; i++) {
-                    $(forms.get(i))
-                        .find('input, textarea, select, label')
-                        .each(function () {
-                            updateElementIndex(this, prefix, i);
-                    });
-                }
+                // Delete the cloneable
+                $(btn).parents('.cloneable').slideUp(300);
+                setTimeout(function() {
+                    // Remove deleted cloneable; update totals/indexes after removal
+                    $(btn).parents('.cloneable').remove();
 
-                // Toggle remove buttons, if necessary
-                toggleRemoveBtn(prefix);
+                    var forms = $('.cloneable'); // Get all the cloneable items
+
+                    // Update the total number of cloneables (1 less than before)
+                    $('#id_' + prefix + '-TOTAL_FORMS').val(forms.length);
+
+                    var i = 0;
+                    // Go through the cloneables and set their indexes, names and IDs
+                    for (formCount=forms.length; i<formCount; i++) {
+                        $(forms.get(i))
+                            .find('input, textarea, select, label')
+                            .each(function () {
+                                updateElementIndex(this, prefix, i);
+                        });
+                    }
+
+                    // Toggle remove buttons, if necessary
+                    toggleRemoveBtn(prefix);
+                }, 300);
             }
             return false;
         };
@@ -195,16 +213,19 @@ $('document').ready(function() {
         // Clone a cloneable element
         var addForm = function(btn, prefix) {
             var formCount = parseInt($('#id_' + prefix + '-TOTAL_FORMS').val(), 10);
+
             // You can only submit a maximum of 12 instances
             if (formCount < $('#id_' + prefix + '-MAX_NUM_FORMS').val()) {
                 // Clone a form from the first form
                 var row = cloneable.clone(false).get(0);
 
-                // Insert it after the last form
-                $(row).removeAttr('id').hide().insertAfter('.cloneable:last').slideDown(300);
-
-                // Relabel or rename all the relevant bits
                 $(row)
+                    // Insert it after the last form
+                    .removeAttr('id')
+                    .hide()
+                    .insertAfter('.cloneable:last')
+                    .slideDown(300)
+                    // Relabel or rename all the relevant bits
                     .find('input, textarea, select, label')
                     .each(function () {
                         updateElementIndex(this, prefix, formCount);
@@ -215,9 +236,9 @@ $('document').ready(function() {
                 $(row).find('.remove-instance').click(function () {
                     return deleteForm(this, prefix);
                 });
-
                 // Add event handlers for date/time widgets
-                
+                initiateDatePickers($(row).find('.field-date'));
+                initiateTimePickers($(row).find('.field-time'));
 
                 // Update the total form count
                 $('#id_' + prefix + '-TOTAL_FORMS').val(formCount + 1);
