98d6a154df1a5d18b17677d834b04c7801cb2f64
Jo Greybill <jogreybill@gmail.com>
2013-08-07 15:13:54 -0400

    Remove unused import

--- apps/events/models/event.py
+++ apps/events/models/event.py
@@ -4,7 +4,6 @@ from dateutil import rrule
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.db.models import Q
 from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
