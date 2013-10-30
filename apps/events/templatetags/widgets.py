926a3a08dd043751b36f43ac1d053ea9ec63cd57
Jo Greybill <jogreybill@gmail.com>
2013-10-30 12:36:52 -0400

    Display event date ranges correctly on mini calendar; make sure month_calendar_map maintains its declared order; check for correct relative_day date format

--- apps/events/templatetags/widgets.py
+++ apps/events/templatetags/widgets.py
@@ -1,6 +1,6 @@
 from datetime import date
 from datetime import datetime
-from datetime import timedelta
+from dateutil import rrule
 import calendar as calgenerator
 import itertools
 import collections
@@ -9,11 +9,10 @@ from django import template
 from django.template import Context
 from django.template import loader
 from django.utils.safestring import mark_safe
+from django.utils.datastructures import SortedDict
 from django.conf import settings
 from dateutil.relativedelta import relativedelta
 
-from events.functions import chunk
-from events.functions import get_date_event_map
 from events.models import Calendar
 
 register = template.Library()
@@ -22,21 +21,41 @@ register = template.Library()
 @register.simple_tag
 def calendar_widget(calendars, day=None, is_manager=0):
 
-    if day is None:
+    if day is None or day is "":
         relative_day = date.today()
     else:
-        relative_day = day
+        if isinstance(day, datetime):
+            relative_day = day.date()
+        elif isinstance(day, date):
+            relative_day = day
+        else:
+            raise TypeError('day must be a datetime.date or datetime.datetime, not a %s' % type(day))
 
-    year = relative_day.year
-    month = relative_day.month
+    # Get next and last month (1st day of month)
+    this_month = date(relative_day.year, relative_day.month, 1)
+    next_month = date((this_month + relativedelta(months=+1)).year, (this_month + relativedelta(months=+1)).month, 1)
+    last_month = date((this_month + relativedelta(months=-1)).year, (this_month + relativedelta(months=-1)).month, 1)
+    months = list([last_month, this_month, next_month])
 
-    # Find date range for the passed month, year combo.  End is defined by
-    # the start of next month minus 1 second.
-    start = datetime(year, month, 1)
-    end = datetime(year if month != 12 else year + 1,
-                   month + 1 if month != 12 else 1,
-                   1) - timedelta(seconds=1)
+    # Create new lists of days in each month (strip week grouping)
+    this_month_cal = list(itertools.chain.from_iterable(calgenerator.Calendar(0).monthdatescalendar(this_month.year, this_month.month)))
+    next_month_cal = list(itertools.chain.from_iterable(calgenerator.Calendar(0).monthdatescalendar(next_month.year, next_month.month)))
+    last_month_cal = list(itertools.chain.from_iterable(calgenerator.Calendar(0).monthdatescalendar(last_month.year, last_month.month)))
 
+    # Set dates as dict keys. Use OrderedDict to sort by date.
+    this_month_cal = collections.OrderedDict((v, []) for k, v in enumerate(this_month_cal))
+    next_month_cal = collections.OrderedDict((v, []) for k, v in enumerate(next_month_cal))
+    last_month_cal = collections.OrderedDict((v, []) for k, v in enumerate(last_month_cal))
+
+    # Create map using SortedDict to maintain declared order.
+    month_calendar_map = SortedDict([(last_month, last_month_cal), (this_month, this_month_cal), (next_month, next_month_cal)])
+
+
+    # Get a date range by which we will fetch events
+    start = last_month_cal.keys()[0]
+    end = next_month_cal.keys()[-1]
+
+    # Fetch events; group them by date
     calendar = None
     events = list()
     if (isinstance(calendars, Calendar)):
@@ -46,33 +65,13 @@ def calendar_widget(calendars, day=None, is_manager=0):
         for cal in calendars:
             events.extend(cal.range_event_instances(start, end).order_by('start'))
 
-    # Getting next and last month makes the assumption that moving 45 days
-    # from the start or 15 days before start will result in next and last
-    # month dates, so start needs to be the start of this month or this needs
-    # to change
-    #this_month = start
-    #next_month = start + timedelta(days=45)
-    #last_month = start - timedelta(days=15)
-
-    this_month = date(start.year, start.month, 1)
-    next_month = this_month + relativedelta(months=+1)
-    last_month = this_month + relativedelta(months=-1)
-
-    # Create new lists of days in each month (strip week grouping)
-    this_month_cal = list(itertools.chain.from_iterable(calgenerator.Calendar(0).monthdatescalendar(this_month.year, this_month.month)))
-    next_month_cal = list(itertools.chain.from_iterable(calgenerator.Calendar(0).monthdatescalendar(next_month.year, next_month.month)))
-    last_month_cal = list(itertools.chain.from_iterable(calgenerator.Calendar(0).monthdatescalendar(last_month.year, last_month.month)))
-
-    # Set dates as dict keys
-    this_month_cal = collections.OrderedDict((v, []) for k, v in enumerate(this_month_cal))
-    next_month_cal = collections.OrderedDict((v, []) for k, v in enumerate(next_month_cal))
-    last_month_cal = collections.OrderedDict((v, []) for k, v in enumerate(last_month_cal))
-
-    month_calendar_map = dict({last_month.month: last_month_cal, this_month.month: this_month_cal, next_month.month: next_month_cal})
-
-
     for event in events:
-        month_calendar_map[event.start.month][event.start.date()].append(event)
+        # Assign event to all days the event falls on
+        rule = rrule.rrule(rrule.DAILY, dtstart=event.start, until=event.end)
+        for event_date in rule:
+            for key in months:
+                if event_date.date() in month_calendar_map[key].keys():
+                    month_calendar_map[key][event_date.date()].append(event)
 
 
     template = loader.get_template('events/widgets/calendar.html')
