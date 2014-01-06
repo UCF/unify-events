5e5d9425c45d72048b2feaa67327950f75df8e3c
Jo Greybill <jogreybill@gmail.com>
2014-01-06 14:10:25 -0500

    Added All Users admin view; added ability to add/remove superuser abilities

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'profiles.views',
-
-    url(r'^settings/$', 'settings', name='profile-settings'),
+    url(r'^$', view='list', name='profile-list'),
+    url(r'^settings/$', view='settings', name='profile-settings'),
+    url(r'^user/(?P<user_id>\d+)/demote', view='update_permissions', name='profile-demote', kwargs={'permissions':False}),
+    url(r'^user/(?P<user_id>\d+)/promote', view='update_permissions', name='profile-promote', kwargs={'permissions':True}),
 )
--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -2,10 +2,18 @@ from datetime import datetime
 import logging
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib import messages
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseForbidden
+from django.http import HttpResponseNotFound
 from django.http import HttpResponseRedirect
 from django.views.generic.simple import direct_to_template
+
+from profiles.models import Profile
 
 from profiles.forms import UserForm, ProfileForm
 
@@ -51,3 +59,50 @@ def settings(request):
         ctx['forms']['profile'] = ProfileForm(instance=request.user.profile, prefix='profile')
 
     return direct_to_template(request, tmpl, ctx)
+
+@login_required
+def list(request):
+    if not request.user.is_superuser:
+        return HttpResponseForbidden('You do not have permission to access this page.')
+
+    ctx = {
+        'users': User.objects.all(),
+    }
+    tmpl = 'events/manager/profiles/list.html'
+
+    # Pagination
+    if ctx['users'] is not None:
+        paginator = Paginator(ctx['users'], 20)
+        page = request.GET.get('page', 1)
+        try:
+            ctx['users'] = paginator.page(page)
+        except PageNotAnInteger:
+            ctx['users'] = paginator.page(1)
+        except EmptyPage:
+            ctx['users'] = paginator.page(paginator.num_pages)
+
+    return direct_to_template(request, tmpl, ctx)
+
+@login_required
+def update_permissions(request, user_id=None, permissions=False):
+    if not isinstance(permissions, bool):
+        return HttpResponseNotFound('Invalid modification to the user was specified.')
+
+    try:
+        modified_user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return HttpResponseNotFound('The user you are trying to modify does not exist.')
+    else:
+        if not request.user.is_superuser:
+            return HttpResponseForbidden('You cannot modify the specified user.')
+        try:
+            modified_user.is_staff = permissions
+            modified_user.is_superuser = permissions
+            modified_user.save()
+        except Exception, e:
+            log.error(str(e))
+            messages.error(request, 'Updating user permissions failed.')
+        else:
+            messages.success(request, 'User permissions successfully updated.')
+
+    return HttpResponseRedirect(reverse('profile-list'))
