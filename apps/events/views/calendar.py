ddfcc8c1de180cd12390107cd934cfb7f2a58798
Jo Greybill <jogreybill@gmail.com>
2014-02-17 12:01:51 -0500

    Added pagination to category, tag front view event lists

--- apps/events/views/calendar.py
+++ apps/events/views/calendar.py
@@ -1,5 +1,8 @@
 MODULE = __import__(__name__)
 
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
 from django.http import Http404, HttpResponse
 from django.template import TemplateDoesNotExist
 from datetime import date, timedelta
@@ -333,6 +336,16 @@ def tag(request, tag, calendar=None, format=None):
         'format': format,
     }
 
+    # Pagination
+    paginator = Paginator(context['events'], 20)
+    page = request.GET.get('page', 1)
+    try:
+        context['events'] = paginator.page(page)
+    except PageNotAnInteger:
+        context['events'] = paginator.page(1)
+    except EmptyPage:
+        context['events'] = paginator.page(paginator.num_pages)
+
     try:
         return direct_to_template(request, template, context, mimetype=format_to_mimetype(format))
     except TemplateDoesNotExist:
@@ -363,6 +376,16 @@ def category(request, category, calendar=None, format=None):
         'events': events,
         'format': format,
     }
+
+    # Pagination
+    paginator = Paginator(context['events'], 20)
+    page = request.GET.get('page', 1)
+    try:
+        context['events'] = paginator.page(page)
+    except PageNotAnInteger:
+        context['events'] = paginator.page(1)
+    except EmptyPage:
+        context['events'] = paginator.page(paginator.num_pages)
 
     try:
         return direct_to_template(request, template, context, mimetype=format_to_mimetype(format))
