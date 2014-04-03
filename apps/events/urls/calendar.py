d68ed19498aef0c74f09f50da9bb65248853840d
Jo Dickson <jogreybill@gmail.com>
2014-04-03 16:13:08 -0400

    Updated 'format' param to require 'feed.' in front of it in url structures

--- apps/events/urls/calendar.py
+++ apps/events/urls/calendar.py
@@ -15,25 +15,25 @@ from events.views.event_views import YearEventsListView
 
 urlpatterns = patterns('events.views.event_views',
     # http://events.ucf.edu/calendar/athletics/event-20404/football-ucf-at-fsu
-    # http://events.ucf.edu/calendar/athletics/event-20404/football-ucf-at-fsu/rss
-    url(r'^(?P<calendar>[\w-]+)/event-(?P<pk>[\d]+)/([\w-]+/)?(?P<format>[\w]+)?$',
+    # http://events.ucf.edu/calendar/athletics/event-20404/football-ucf-at-fsu/feed.rss
+    url(r'^(?P<calendar>[\w-]+)/event-(?P<pk>[\d]+)/([\w-]+/)?(feed\.(?P<format>[\w]+))?$',
         EventDetailView.as_view(),
         name='event'
     ),
 
     # http://events.ucf.edu/calendar/athletics
-    # http://events.ucf.edu/calendar/athletics/2010/json
+    # http://events.ucf.edu/calendar/athletics/2010/feed.json
     # http://events.ucf.edu/calendar/athletics/2010/01
-    # http://events.ucf.edu/calendar/athletics/2010/01/10/rss
-    url(r'^(?P<calendar>[\w-]+)/(?P<format>[\w]+)?$', view=DayEventsListView.as_view(), name='calendar'),
-    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<format>[\w]+)?$', YearEventsListView.as_view(), name='year-listing'),
-    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<format>[\w]+)?$', MonthEventsListView.as_view(), name='month-listing'),
-    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<day>[\d]+)/(?P<format>[\w]+)?$', DayEventsListView.as_view(), name='day-listing'),
-    url(r'^(?P<calendar>[\w-]+)/week-of/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<day>[\d]+)/(?P<format>[\w]+)?$', WeekEventsListView.as_view(), name='week-listing'),
+    # http://events.ucf.edu/calendar/athletics/2010/01/10/feed.rss
+    url(r'^(?P<calendar>[\w-]+)/(feed\.(?P<format>[\w]+))?$', view=DayEventsListView.as_view(), name='calendar'),
+    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(feed\.(?P<format>[\w]+))?$', YearEventsListView.as_view(), name='year-listing'),
+    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/(feed\.(?P<format>[\w]+))?$', MonthEventsListView.as_view(), name='month-listing'),
+    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<day>[\d]+)/(feed\.(?P<format>[\w]+))?$', DayEventsListView.as_view(), name='day-listing'),
+    url(r'^(?P<calendar>[\w-]+)/week-of/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<day>[\d]+)/(feed\.(?P<format>[\w]+))?$', WeekEventsListView.as_view(), name='week-listing'),
 
     # TODO replace with list view?
     # http://events.ucf.edu/calendar/athletics/from/2010-01-02/to/2010-02-02
-    url(r'^(?P<calendar>[\w-]+)/from/(?P<start>[\w-]+)/to/(?P<end>[\w-]+)/(?P<format>[\w]+)?$',
+    url(r'^(?P<calendar>[\w-]+)/from/(?P<start>[\w-]+)/to/(?P<end>[\w-]+)/(feed\.(?P<format>[\w]+))?$',
         view=range_listing,
         name='range-listing'
     ),
@@ -41,19 +41,19 @@ urlpatterns = patterns('events.views.event_views',
     # http://events.ucf.edu/calendar/athletics/this-year
     # http://events.ucf.edu/calendar/athletics/today
     # etc.
-    url(r'^(?P<calendar>[\w-]+)/(?P<type>[\w-]+)/(?P<format>[\w]+)?$',
+    url(r'^(?P<calendar>[\w-]+)/(?P<type>[\w-]+)/(feed\.(?P<format>[\w]+))?$',
         view=named_listing,
         name='named-listing'
     ),
 
     # http://events.ucf.edu/calendar/athletics/tag/tag-name
-    url(r'^(?P<calendar>[\w-]+)/tag/(?P<tag>[\w-]+)/(?P<format>[\w]+)?$',
+    url(r'^(?P<calendar>[\w-]+)/tag/(?P<tag>[\w-]+)/(feed\.(?P<format>[\w]+))?$',
         view=EventsByTagList.as_view(),
         name='tag-by-calendar'
     ),
 
     # http://events.ucf.edu/calendar/athletics/category/category-name
-    url(r'^(?P<calendar>[\w-]+)/category/(?P<category>[\w-]+)/(?P<format>[\w]+)?$',
+    url(r'^(?P<calendar>[\w-]+)/category/(?P<category>[\w-]+)/(feed\.(?P<format>[\w]+))?$',
         view=EventsByCategoryList.as_view(),
         name='category-by-calendar'
     ),
