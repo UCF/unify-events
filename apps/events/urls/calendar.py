e0fc842534dfc3ae8f429769c3184fd5f5da8d2a
Jo Dickson <jogreybill@gmail.com>
2014-04-03 15:14:32 -0400

    quotation mark consistency

--- apps/events/urls/calendar.py
+++ apps/events/urls/calendar.py
@@ -26,16 +26,16 @@ urlpatterns = patterns('events.views.event_views',
     # http://events.ucf.edu/calendar/athletics/2010/01
     # http://events.ucf.edu/calendar/athletics/2010/01/10/rss
     url(r'^(?P<calendar>[\w-]+)/(?P<format>[\w]+)?$', view=DayEventsListView.as_view(), name='calendar'),
-    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<format>[\w]+)?$', YearEventsListView.as_view(), name="year-listing"),
-    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<format>[\w]+)?$', MonthEventsListView.as_view(), name="month-listing"),
-    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<day>[\d]+)/(?P<format>[\w]+)?$', DayEventsListView.as_view(), name="day-listing"),
+    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<format>[\w]+)?$', YearEventsListView.as_view(), name='year-listing'),
+    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<format>[\w]+)?$', MonthEventsListView.as_view(), name='month-listing'),
+    url(r'^(?P<calendar>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<day>[\d]+)/(?P<format>[\w]+)?$', DayEventsListView.as_view(), name='day-listing'),
     url(r'^(?P<calendar>[\w-]+)/week-of/(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<day>[\d]+)/(?P<format>[\w]+)?$', WeekEventsListView.as_view(), name='week-listing'),
 
     # TODO replace with list view?
     # http://events.ucf.edu/calendar/athletics/from/2010-01-02/to/2010-02-02
     url(r'^(?P<calendar>[\w-]+)/from/(?P<start>[\w-]+)/to/(?P<end>[\w-]+)/(?P<format>[\w]+)?$',
         view=range_listing,
-        name="range-listing"
+        name='range-listing'
     ),
 
     # http://events.ucf.edu/calendar/athletics/this-year
@@ -43,7 +43,7 @@ urlpatterns = patterns('events.views.event_views',
     # etc.
     url(r'^(?P<calendar>[\w-]+)/(?P<type>[\w-]+)/(?P<format>[\w]+)?$',
         view=named_listing,
-        name="named-listing"
+        name='named-listing'
     ),
 
     # http://events.ucf.edu/calendar/athletics/tag/tag-name
