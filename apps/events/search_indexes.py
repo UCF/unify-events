3ccfe9c1be2edecc40b101cfafdf7a7a1afa5d21
Jo Dickson <jogreybill@gmail.com>
2014-04-07 15:51:19 -0400

    Added calendars to search index; updated basic search results list

--- apps/events/search_indexes.py
+++ apps/events/search_indexes.py
@@ -1,11 +1,12 @@
 from haystack import indexes
+from events.models import Calendar
 from events.models import State
 from events.models import Event
+
 
 class EventIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     calendar = indexes.CharField(model_attr='calendar')
-    title = indexes.CharField(model_attr='title')
     description = indexes.CharField(model_attr='description')
     category = indexes.CharField(model_attr='category')
     tags = indexes.CharField(model_attr='tags')
@@ -20,3 +21,19 @@ class EventIndex(indexes.SearchIndex, indexes.Indexable):
         (no pending or rereview events; allow canceled.)
         """
         return self.get_model().objects.filter(state=State.posted)
+
+
+class CalendarIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    description = indexes.CharField(model_attr='description')
+
+    def get_model(self):
+        return Calendar
+
+    def index_queryset(self, using=None):
+        """
+        Used when the entire index for model is updated.
+        Only retrieve published events that are published
+        (no pending or rereview events; allow canceled.)
+        """
+        return self.get_model().objects.all()
