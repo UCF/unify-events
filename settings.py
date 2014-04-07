be6451871643b2f8e0a851ba6e7dbb9e03dd8af3
Jo Dickson <jogreybill@gmail.com>
2014-04-07 12:21:07 -0400

    Added note about replacing RealtimeSignalProcessor w/cron

--- settings.py
+++ settings.py
@@ -163,6 +163,8 @@ HAYSTACK_CONNECTIONS = {
 }
 # Enables updating of models with an associated SearchIndex
 # when that model is saved or deleted.
+# TODO: replace w/cron job for fewer index rebuilds:
+# http://django-haystack.readthedocs.org/en/v2.1.0/tutorial.html#reindex
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 
