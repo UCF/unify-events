e3a257ae9064e6b9bae7a6348fa579d350536cb7
Jo Dickson <jogreybill@gmail.com>
2014-04-18 09:34:05 -0400

    Removed wysihtml5 image insertion icon

--- static/js/script.js
+++ static/js/script.js
@@ -282,7 +282,7 @@ var initiateWysiwyg = function(textarea) {
         "lists": true, // (Un)ordered lists, e.g. Bullets, Numbers. Default true
         "html": false, // Button which allows you to edit the generated HTML. Default false
         "link": true, // Button to insert a link. Default true
-        "image": true, // Button to insert an image. Default true,
+        "image": false, // Button to insert an image. Default true,
         "color": false, // Button to change color of font
         events: {
             "load": function() {
