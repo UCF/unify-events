a563ced6ed74fe4fb1fb78e366c961ce116840ee
Jo Dickson <jogreybill@gmail.com>
2014-06-27 12:46:00 -0400

    Prevent /search/ from being picked up as main calendar 'named-listing' url

--- urls.py
+++ urls.py
@@ -15,9 +15,6 @@ from events.views.event_views import YearEventsListView
 
 admin.autodiscover()
 
-# Get main calendar so we have access to its slug:
-main_calendar = Calendar.objects.get(pk=settings.FRONT_PAGE_CALENDAR_PK)
-
 baseurlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     url(r'^manager/', include('events.urls.manager')),
@@ -25,7 +22,40 @@ baseurlpatterns = patterns('',
     url(r'^event/', include('events.urls.event_urls')),
     url(r'^category/', include('events.urls.category')),
     url(r'^tag/', include('events.urls.tag')),
-    # Main calendar url exceptions
+    url(r'^help/$', TemplateView.as_view(template_name='events/static/help.html'), name='help'),
+    url(r'for-developers/$', TemplateView.as_view(template_name='events/static/for-developers.html'), name='for-developers'),
+    # TODO: production-ready static file delivery
+    url(r'^tools/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT + '/events-widget/'}),
+    url(r'^calendar-widget/(?P<view>[\w-]+)/(?P<size>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/$', TemplateView.as_view(template_name='events/widgets/calendar-by-url.html'), name='calendar-widget'),
+    url(r'^calendar-widget/(?P<view>[\w-]+)/(?P<pk>\d+)/(?P<calendar_slug>[\w-]+)/(?P<size>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/$', TemplateView.as_view(template_name='events/widgets/calendar-by-url.html'), name='calendar-widget-by-calendar'),
+    url(r'^calendar-widget/(?P<view>[\w-]+)/(?P<calendar_slug>[\w-]+)/(?P<size>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/$', TemplateView.as_view(template_name='events/widgets/calendar-by-url.html'), name='calendar-widget-by-calendar'),
+    url(r'^esi/template/(?P<path>.*)', view='core.views.esi_template', name='esi-template'),
+)
+
+
+# Add static file location for debug
+baseurlpatterns += staticfiles_urlpatterns()
+urlpatterns = patterns('',
+    (r'^', include(baseurlpatterns)),
+)
+
+
+# Append search urls (this MUST go before Main Calendar overrides; else a 404 is returned on the haystack_search view!)
+if settings.SEARCH_ENABLED:
+    urlpatterns += patterns('',
+        url(r'^search/', include('haystack.urls')),
+    )
+else:
+    urlpatterns += patterns('',
+        url(r'^search/', DayEventsListView.as_view(), kwargs={'pk': settings.FRONT_PAGE_CALENDAR_PK}, name='haystack_search'),
+    )
+
+
+# Get Main Calendar so we have access to its slug:
+main_calendar = Calendar.objects.get(pk=settings.FRONT_PAGE_CALENDAR_PK)
+
+# Append Main Calendar url overrides
+urlpatterns += patterns('',
     url(r'^(feed\.(?P<format>[\w]+))?$',
         DayEventsListView.as_view(),
         kwargs={'pk': settings.FRONT_PAGE_CALENDAR_PK, 'slug': main_calendar.slug},
@@ -56,32 +86,9 @@ baseurlpatterns = patterns('',
         kwargs={'pk': settings.FRONT_PAGE_CALENDAR_PK, 'slug': main_calendar.slug},
         name='main-calendar-named-listing'
     ),
-    # Static pages
-    url(r'^help/$', TemplateView.as_view(template_name='events/static/help.html'), name='help'),
-    url(r'for-developers/$', TemplateView.as_view(template_name='events/static/for-developers.html'), name='for-developers'),
-    # TODO: production-ready static file delivery
-    url(r'^tools/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT + '/events-widget/'}),
-    url(r'^calendar-widget/(?P<view>[\w-]+)/(?P<size>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/$', TemplateView.as_view(template_name='events/widgets/calendar-by-url.html'), name='calendar-widget'),
-    url(r'^calendar-widget/(?P<view>[\w-]+)/(?P<pk>\d+)/(?P<calendar_slug>[\w-]+)/(?P<size>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/$', TemplateView.as_view(template_name='events/widgets/calendar-by-url.html'), name='calendar-widget-by-calendar'),
-    url(r'^calendar-widget/(?P<view>[\w-]+)/(?P<calendar_slug>[\w-]+)/(?P<size>[\w-]+)/(?P<year>[\d]+)/(?P<month>[\d]+)/$', TemplateView.as_view(template_name='events/widgets/calendar-by-url.html'), name='calendar-widget-by-calendar'),
-    url(r'^esi/template/(?P<path>.*)', view='core.views.esi_template', name='esi-template'),
 )
 
-# Add static file location for debug
-baseurlpatterns += staticfiles_urlpatterns()
 
-urlpatterns = patterns('',
-    (r'^', include(baseurlpatterns)),
-)
-
+# Status code handling
 handler500 = TemplateView.as_view(template_name='events/static/500.html')
 handler404 = TemplateView.as_view(template_name='events/static/404.html')
-
-if settings.SEARCH_ENABLED:
-    urlpatterns += patterns('',
-        url(r'^search/', include('haystack.urls')),
-    )
-else:
-    urlpatterns += patterns('',
-        url(r'^search/', DayEventsListView.as_view(), kwargs={'pk': settings.FRONT_PAGE_CALENDAR_PK}, name='haystack_search'),
-    )
