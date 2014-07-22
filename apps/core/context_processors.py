e204eb3bab8bc89ea69e64d11792512593ac183d
Jo Dickson <jogreybill@gmail.com>
2014-07-22 12:27:31 -0400

    Set canonical in context processors

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -1,8 +1,13 @@
 from django.conf import settings
 
+def get_canonical(request):
+    return settings.CANONICAL_ROOT + request.path
+
 def global_settings(request):
 	# Return any necessary settings values.
 	return {
 		'GA_ACCOUNT': settings.GA_ACCOUNT,
-		'FALLBACK_EVENT_DESCRIPTION': settings.FALLBACK_EVENT_DESCRIPTION
+		'FALLBACK_EVENT_DESCRIPTION': settings.FALLBACK_EVENT_DESCRIPTION,
+		'CANONICAL_ROOT': settings.CANONICAL_ROOT,
+		'CANONICAL_URL': get_canonical(request)
 	}
