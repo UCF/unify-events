4f448ea79d6830c43a4a714264288f5ffb06e8ee
Jo Dickson <jogreybill@gmail.com>
2014-07-23 12:48:13 -0400

    Missing comma

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -7,7 +7,7 @@ def global_settings(request):
 	# Return any necessary settings values.
 	return {
 		'GA_ACCOUNT': settings.GA_ACCOUNT,
-		'GOOGLE_WEBMASTER_VERIFICATION': settings.GOOGLE_WEBMASTER_VERIFICATION
+		'GOOGLE_WEBMASTER_VERIFICATION': settings.GOOGLE_WEBMASTER_VERIFICATION,
 		'FALLBACK_EVENT_DESCRIPTION': settings.FALLBACK_EVENT_DESCRIPTION,
 		'CANONICAL_ROOT': settings.CANONICAL_ROOT,
 		'CANONICAL_URL': get_canonical(request)
