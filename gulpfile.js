f49802eafa2af6a4c2f3e350510dc8aafa587406
Jo Dickson <jogreybill@gmail.com>
2015-08-11 16:00:06 -0500

    Converted all indents to spaces

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,52 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	minifyCss = require('gulp-minify-css'),
-	bless = require('gulp-bless'),
-	notify = require('gulp-notify'),
-	bower = require('gulp-bower'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-	jshint = require('gulp-jshint'),
-	jshintStylish = require('jshint-stylish'),
-	scsslint = require('gulp-scss-lint'),
-	vinylPaths = require('vinyl-paths');
-	// browserSync = require('browser-sync').create(),
-	// reload = browserSync.reload;
+  sass = require('gulp-sass'),
+  minifyCss = require('gulp-minify-css'),
+  bless = require('gulp-bless'),
+  notify = require('gulp-notify'),
+  bower = require('gulp-bower'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  jshint = require('gulp-jshint'),
+  jshintStylish = require('jshint-stylish'),
+  scsslint = require('gulp-scss-lint'),
+  vinylPaths = require('vinyl-paths');
+  // browserSync = require('browser-sync').create(),
+  // reload = browserSync.reload;
 
 var config = {
-	sassPath: './static_files/assets/scss',
-	cssPath: './static_files/static/css',
-	jsPath: './static_files/assets/js',
-	jsMinPath: './static_files/static/js',
-	fontPath: './static_files/static/fonts',
-	bowerDir: './static_files/assets/components'
+  sassPath: './static_files/assets/scss',
+  cssPath: './static_files/static/css',
+  jsPath: './static_files/assets/js',
+  jsMinPath: './static_files/static/js',
+  fontPath: './static_files/static/fonts',
+  bowerDir: './static_files/assets/components'
 };
 
 gulp.task('bower', function() {
-	bower()
-		.pipe(gulp.dest(config.bowerDir))
-		.on('end', function() {
+  bower()
+    .pipe(gulp.dest(config.bowerDir))
+    .on('end', function() {
       // Copy gylphicon font files out to config.fontPath
-			gulp.src(config.bowerDir + '/bootstrap-sass-official/assets/fonts/bootstrap/*')
-				.pipe(gulp.dest(config.fontPath));
+      gulp.src(config.bowerDir + '/bootstrap-sass-official/assets/fonts/bootstrap/*')
+        .pipe(gulp.dest(config.fontPath));
 
       // Copy fontawesome font files out to config.fontPath
-			gulp.src(config.bowerDir + '/font-awesome/fonts/*')
-				.pipe(gulp.dest(config.fontPath));
-		});
+      gulp.src(config.bowerDir + '/font-awesome/fonts/*')
+        .pipe(gulp.dest(config.fontPath));
+    });
 });
 
 gulp.task('css', function() {
   // style.min.css
-	gulp.src(config.sassPath + '/*.scss')
-		.pipe(scsslint())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(minifyCss({compatibility: 'ie8'}))
-		.pipe(rename('style.min.css'))
-		.pipe(bless())
-		.pipe(gulp.dest(config.cssPath));
-		//.pipe(browserSync.stream());
+  gulp.src(config.sassPath + '/*.scss')
+    .pipe(scsslint())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename('style.min.css'))
+    .pipe(bless())
+    .pipe(gulp.dest(config.cssPath));
+    //.pipe(browserSync.stream());
 
   // style-backend.min.css
   gulp.src([
@@ -62,37 +62,37 @@ gulp.task('css', function() {
 
 gulp.task('js', function() {
   // lint scripts in config.jsPath
-	gulp.src([config.jsPath + '/*.js', '!' + config.jsPath + '/*.min.js'])
-		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish'))
-		.pipe(jshint.reporter('fail'));
+  gulp.src([config.jsPath + '/*.js', '!' + config.jsPath + '/*.min.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 
   // script.min.js
-	gulp.src([
+  gulp.src([
     config.bowerDir + '/jquery-placeholder/jquery.placeholder.js',
     config.bowerDir + '/bootstrap-sass-official/assets/javascripts/bootstrap.min.js',
     config.jsPath + '/script.js'
   ])
-		.pipe(concat('script.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest(config.jsMinPath));
+    .pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.jsMinPath));
 
   // script-frontend.min.js
-	gulp.src(config.jsPath + '/script-frontend.js')
-		.pipe(rename('script-frontend.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest(config.jsMinPath));
+  gulp.src(config.jsPath + '/script-frontend.js')
+    .pipe(rename('script-frontend.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.jsMinPath));
 
   // script-backend.min.js
-	gulp.src([
+  gulp.src([
     config.bowerDir + '/bootstrap3-typeahead/bootstrap3-typeahead.js',
     config.bowerDir + '/jquery-timepicker-jt/jquery.timepicker.js',
     config.bowerDir + '/bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
     config.jsPath + '/script-manager.js'
   ])
-		.pipe(concat('script-manager.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest(config.jsMinPath));
+    .pipe(concat('script-manager.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.jsMinPath));
 
   // wysiwyg.min.js
   gulp.src([
@@ -105,8 +105,8 @@ gulp.task('js', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch(config.sassPath + '/*.scss', ['css']);
-	gulp.watch(config.jsPath + '/*.js', ['js']);
+  gulp.watch(config.sassPath + '/*.scss', ['css']);
+  gulp.watch(config.jsPath + '/*.js', ['js']);
 });
 
 gulp.task('default', ['bower', 'css', 'js']);
