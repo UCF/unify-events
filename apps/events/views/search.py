67014d7191dbdb376555a86c2c8477219b9e6ad0
Jo Dickson <jogreybill@gmail.com>
2020-02-24 14:46:30 -0500

    Updated global search view to use new class-based views in django-haystack 2.4.x; added separate json feed template for global search

--- apps/events/views/search.py
+++ apps/events/views/search.py
@@ -1,18 +1,21 @@
 import logging
 
-from haystack.views import SearchView
+from haystack.generic_views import SearchView
 
+from core.views import MultipleFormatTemplateViewMixin
 from events.models import Event
 
 log = logging.getLogger(__name__)
 
 
-class GlobalSearchView(SearchView):
+class GlobalSearchView(MultipleFormatTemplateViewMixin, SearchView):
+    template_name = 'search/search.'
+    available_formats = ['html', 'json']
+
     """
     Only return unique events (do not return events copied to other calendars.)
     """
-    def get_results(self):
-        results = super(GlobalSearchView, self).get_results()
-        results = results.filter(created_from='None') # Yes, this has to be a string. Haystack will not return results by NoneType.
-
-        return results
+    def get_queryset(self):
+        queryset = super(GlobalSearchView, self).get_queryset()
+        queryset = queryset.filter(created_from='None') # Yes, this has to be a string. Haystack will not return results by NoneType
+        return queryset
