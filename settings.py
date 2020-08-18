9f5cb70ad9502e905aab82978bccfe74170bacdb
Jo Dickson <jogreybill@gmail.com>
2020-08-18 14:37:09 -0400

    Appended BASE_DIR to template directory path to fix debugging issues

--- settings.py
+++ settings.py
@@ -145,7 +145,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'templates'
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
