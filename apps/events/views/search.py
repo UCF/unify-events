9908ef55b63092cd9aebbc785f317b46e8f460fe
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-08-19 14:47:58 -0400

    Added calendars to results

--- apps/events/views/search.py
+++ apps/events/views/search.py
@@ -16,12 +16,18 @@ class GlobalSearchView(MultipleFormatTemplateViewMixin, ListView):
     Only return unique events (do not return events copied to other calendars.)
     """
     def get_queryset(self):
-        query = self.request.GET.get('q')
-        queryset = Event.objects.filter(title__icontains=query).filter(created_from=None)
+        query = self.request.GET.get('q', None)
+        if query:
+            queryset = Event.objects.filter(title__icontains=query).filter(created_from=None)
+        else:
+            queryset = Event.objects.none()
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super(GlobalSearchView, self).get_context_data(**kwargs)
         context['query'] = self.request.GET.get('q')
-        context['calendars'] = Calendar.objects.filter(title__icontains=context['query'])
+        if context['query']:
+            context['calendars'] = Calendar.objects.filter(title__icontains=context['query'])
+        else:
+            context['calendars'] = Calendar.objects.none()
         return context
