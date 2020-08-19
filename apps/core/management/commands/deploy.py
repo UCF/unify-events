b6621234ca84a5c9b39a85c41657a8e5e8cb9a6d
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-08-19 10:10:48 -0400

    Added deploy command to core

---
+++ apps/core/management/commands/deploy.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
+
+from django.db.migrations.recorder import MigrationRecorder
+from django.db import connection
+
+class Command(BaseCommand):
+    help = 'Runs deployment related tasks.'
+
+    def handle(self, *args, **options):
+        migration_count = MigrationRecorder.Migration.objects.filter(applied__isnull=False).count()
+        all_tables = connection.introspection.table_names()
+
+        # Tables exists, but there are no migrations run
+        if migration_count == 0 and 'events_event' in all_tables:
+            call_command('migrate', '--fake-initial')
+            self.stdout.write("Running initial migrations")
+        else:
+            call_command('migrate')
+            self.stdout.write("Running migrations")
+
+        call_command('collectstatic', '--link', '--no-input')
+
+        self.stdout.write(self.style.SUCCESS("Finished deploying"))
+
