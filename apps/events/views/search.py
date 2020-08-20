2407327193a70b39066dc81c85f57105f547f785
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-08-20 14:45:04 -0400

    Adjusted search query

--- apps/events/views/search.py
+++ apps/events/views/search.py
@@ -21,11 +21,12 @@ class GlobalSearchView(MultipleFormatTemplateViewMixin, ListView):
     def get_queryset(self):
         query = self.request.GET.get('q', None)
         if query:
+            now = datetime.now().replace(hour=0, minute=0, second=0)
+
             queryset = Event.objects.filter(
                         Q(title__icontains=query) | Q(calendar__title__icontains=query)
-                        ).filter(
-                            state__in=State.get_published_states(),
-                            event_instances__start__gte=datetime.now().replace(hour=0, minute=0, second=0)
+                        ).filter(state__in=State.get_published_states(),
+                        ).filter(Q(event_instances__start__gte=now) | Q(event_instances__end__lte=now) | Q(event_instances__until__gte=now)
                         ).filter(created_from=None)
         else:
             queryset = Event.objects.none()
