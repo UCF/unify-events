96ad4620223be59cf59c33090c9c1972c0294c27
Jim Barnes <jimbarnesdeveloper@gmail.com>
2021-02-19 14:38:03 -0500

    Removed the context data we do not use anymore

--- events/views/manager/event.py
+++ events/views/manager/event.py
@@ -65,8 +65,6 @@ class EventCreate(CreateView):
         """
         context = super(EventCreate, self).get_context_data(**kwargs)
         ctx = {
-               'locations': Location.objects.all(),
-               'tags': Tag.objects.all().order_by('name'),
                'promoted_tags': PromotedTag.objects.all().order_by('tag__name'),
         }
         ctx.update(context)
@@ -190,8 +188,6 @@ class EventUpdate(SuccessPreviousViewRedirectMixin, UpdateView):
         context = super(EventUpdate, self).get_context_data(**kwargs)
 
         ctx = {
-               'locations': Location.objects.all(), # Always pass all locations here so that users can modify events with locations that are in review
-               'tags': Tag.objects.all().order_by('name'),
                'promoted_tags': PromotedTag.objects.all().order_by('tag__name'),
                # Needed to determine whether to show the cancel/un-cancel button
                'posted_state': State.posted
