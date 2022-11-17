04f061f3d02c0d1e0b2ef53e80104467c08cef2b
Jim Barnes <jimbarnesdeveloper@gmail.com>
2022-11-17 14:25:56 -0500

    Initial logic for updating internal links through management command

--- seo/util.py
+++ seo/util.py
@@ -1,13 +1,14 @@
 import re
 import settings
+from datetime import datetime
 
-from django import template
-from seo.models import InternalLink
+from seo.models import InternalLink, InternalLinkRecord
 
-register = template.Library()
+def internal_links(event, process_time):
+    value = event.description
+    if not process_time:
+        process_time = datetime.now()
 
-@register.filter(name='internal_links')
-def internal_links(value, arg=None):
     links = InternalLink.objects.find_in_text(value)
     for link in links:
         phrases = '|'.join(link.phrases.values_list('phrase', flat=True))
@@ -21,6 +22,26 @@ def internal_links(value, arg=None):
                 value,
                 1
             )
+
+            phrase = next(link.phrases.get(phrase=p) for p in phrases.split('|') if p in value)
+            try:
+                record = InternalLinkRecord.objects.get(
+                    keyword_phrase=phrase,
+                    event=event,
+                    internal_link=link
+                )
+                record.updated_at = process_time
+                record.save()
+            except InternalLinkRecord.DoesNotExist:
+                record = InternalLinkRecord(
+                    keyword_phrase=phrase,
+                    event=event,
+                    internal_link=link,
+                    updated_at=process_time
+                )
+                record.save()
+
+
             # Don't do the replacement again if regex replace happened
             continue
         elif match and settings.SEO_AUTO_ANCHORS_FORCE_UPDATE == False:
