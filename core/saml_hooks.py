6a3104320afde06c57916b402c69427d8203503f
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-05-27 11:11:33 -0400

    Removed debugging logging

--- core/saml_hooks.py
+++ core/saml_hooks.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
 
-import logging
-
 def on_saml_user_create(user: User, saml_data: dict):
     user.first_name = saml_data['givenName'][0]
     user.last_name = saml_data['surname'][0]
@@ -9,7 +7,6 @@ def on_saml_user_create(user: User, saml_data: dict):
     user.save()
 
 def on_saml_before_login(user: User, saml_data: dict):
-    logging.info(f"User {user}, {user.pk} is authenticated: {user.is_authenticated}")
     user.first_name = saml_data['givenName'][0]
     user.last_name = saml_data['surname'][0]
     user.email = saml_data['emailAddress'][0]
