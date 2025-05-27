2c4d4385f150c607196df98b50b4f758af38cad9
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-05-27 10:08:57 -0400

    Remove find user hook

--- core/saml_hooks.py
+++ core/saml_hooks.py
@@ -13,19 +13,3 @@ def on_saml_before_login(user: User, saml_data: dict):
     user.last_name = saml_data['surname'][0]
     user.email = saml_data['emailAddress'][0]
     user.save()
-
-def on_saml_find_user(saml_data: dict) -> User:
-    guid = saml_data['http://schemas.microsoft.com/identity/claims/objectidentifier'][0]
-    logging.info(guid)
-    user = None
-    try:
-        user = User.objects.get(profile__guid=guid.strip())
-    except User.DoesNotExist:
-        logging.warning(f"User not found: {guid}")
-        pass
-    except Exception as e:
-        logging.error(f"Error: {e}")
-
-    logging.info(user)
-
-    return user
