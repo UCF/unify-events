08c10fec67db4d291966aa02aed6aa4c2eaaf3d3
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-05-27 10:15:04 -0400

    More debugging messages

--- core/saml_hooks.py
+++ core/saml_hooks.py
@@ -9,6 +9,7 @@ def on_saml_user_create(user: User, saml_data: dict):
     user.save()
 
 def on_saml_before_login(user: User, saml_data: dict):
+    logging.info(f"User {user}, {user.pk} is authenticated: {user.is_authenticated}")
     user.first_name = saml_data['givenName'][0]
     user.last_name = saml_data['surname'][0]
     user.email = saml_data['emailAddress'][0]
