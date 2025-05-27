ddbb6c0d966fea1a3344fd0405524d70fa82d2c0
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-05-27 08:57:53 -0400

    Looking at user data now

--- core/saml_hooks.py
+++ core/saml_hooks.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 import logging
 
 def on_saml_user_create(user: User, saml_data: dict):
-    logging.info(saml_data)
+    logging.info(user)
     user.first_name = saml_data['givenName'][0]
     user.last_name = saml_data['surname'][0]
     user.email = saml_data['emailAddress'][0]
     user.save()
 
 def on_saml_before_login(user: User, saml_data: dict):
-    logging.info(saml_data)
+    logging.info(user)
     user.first_name = saml_data['givenName'][0]
     user.last_name = saml_data['surname'][0]
     user.email = saml_data['emailAddress'][0]
