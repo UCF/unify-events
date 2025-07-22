93a1f23af128ef88663a922123887cc3fa1cb6de
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-07-22 05:18:12 -0400

    Filtering admin for to only main calendar events

--- events/admin.py
+++ events/admin.py
@@ -7,16 +7,27 @@ from events.models import Location
 from events.models import Category
 from events.models import Promotion
 from events.models import FeaturedEvent
+from events.models import get_main_calendar
 
 admin.site.register(Event)
 admin.site.register(EventInstance)
 admin.site.register(Location)
 admin.site.register(Category)
 admin.site.register(Promotion)
-admin.site.register(FeaturedEvent)
 
 @admin.register(Calendar)
 class CalendarAdmin(admin.ModelAdmin):
     search_fields = ('title',)
     list_filter = ('active', 'tier',)
     list_display = ('title', 'active', 'trusted', 'tier',)
+
+
+@admin.register(FeaturedEvent)
+class FeaturedEventAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(FeaturedEventAdmin, self).get_form(request, obj, **kwargs)
+        main_calendar = get_main_calendar()
+        if main_calendar:
+            form.base_fields['event'].queryset = Event.objects.filter(calendar=get_main_calendar())
+
+        return form
