6c36860e53c18a42208d307fd1834fa9cee2cbfe
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-09-29 13:45:50 -0400

    Back porting s3 uploads

--- settings.py
+++ settings.py
@@ -61,11 +61,13 @@ INSTALLED_APPS = (
     'events',
     'unlevents',
     'widget_tweaks',
-    'django_bleach'
+    'django_bleach',
+    'storages'
 )
 
 AUTH_PROFILE_MODULE = 'events.Profile'
-FILE_UPLOAD_PATH = 'uploads'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DATABASE_ROUTERS = ['unlevents.dbrouter.UNLEventsRouter']
 
@@ -88,40 +90,6 @@ TEMPLATES = [
     },
 ]
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/events/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/events/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'static_files/static'),
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
 
 try:
     from settings_local import *
