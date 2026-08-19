39fdf5b84d2a7f6d4dbb5815d0c241106c35458e
Jim Barnes <james.barnes@ucf.edu>
2026-08-19 10:23:47 -0400

    Added additional protections against user input

--- core/views.py
+++ core/views.py
@@ -229,15 +229,21 @@ class PaginationRedirectMixin(object):
         try:
             return super(PaginationRedirectMixin, self).dispatch(request, *args, **kwargs)
         except Http404:
-            page = self.request.GET.get('page')
-            if page and page.isdigit() and int(page) > paginator.num_pages:
+            try:
+                page = int(self.request.GET.get('page'))
+            except (TypeError, ValueError):
+                # No usable page number, so the 404 can't have been caused by
+                # exceeding the maximum number of pages.
+                page = None
+
+            if page is not None and page > paginator.num_pages:
                 # Get the current page url and append the new page number:
                 url = '%s?page=%s' % (reverse(url_name, kwargs=r_kwargs), paginator.num_pages)
                 return HttpResponseRedirect(url)
-            else:
-                # re-raise the original Http404, as the reason for the 404 was
-                # not that the maximum number of pages was exceeded
-                raise
+
+            # re-raise the original Http404, as the reason for the 404 was
+            # not that the maximum number of pages was exceeded
+            raise
 
 
 class InvalidSlugRedirectMixin(object):
